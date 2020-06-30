c1c27c9f3cc4db22bd655a2265f3af38fd49f79e
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-30 10:38:25 -0500

    completed a couple tests

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,5 +1,23 @@
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class MyFirstTest {
+
+    @Test
+    public void isCodeupSameAsCodeUp() {
+        String expected = "CodeUp";
+        String actual = "Codeup";
+        assertNotEquals(expected, actual);
+    }
+
+    @Test
+    public void areArrayListsTheSame() {
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+        assertNotSame(languages, moreLanguages);
+    }
 }
