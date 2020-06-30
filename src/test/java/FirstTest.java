2a984a8ede28dfab2b90901691261d1d935e9406
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-30 10:04:20 -0500

    added more tests

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -4,11 +4,33 @@ import static org.junit.Assert.*;
 public class FirstTest {
     @Test
     public void test2Is2() {
-
+        int input = 2;
+        assertEquals(2, input);
     }
 
     @Test
     public void test3Is3() {
 
     }
+
+    @Test
+    public void testIfCompanyNameIsEquals() {
+        String expected = "Codeup";
+        String actual = "Codeup";
+
+        assertEquals(expected, actual);
+    }
+
+// When you're using floating points values a third parameter is required (Delta) which represents a positive margin of error of decimals.
+
+    @Test
+    public void testIfChangeIsCorrect() {
+        Double price = 10.0;
+        Double discount = 4.5;
+
+        assertEquals(5.5, price - discount, 0);
+        assertEquals(5.1, price - discount, 0.5);
+        assertNotEquals(4.9, price - discount, 0.5);
+    }
+
 }
