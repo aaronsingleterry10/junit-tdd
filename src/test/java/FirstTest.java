b8ad8abf20f31680c1cc3be0c9464203ce5f8d90
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-30 09:42:03 -0500

    first commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FirstTest {
+    @Test
+    public void test2Is2() {
+
+    }
+
+    @Test
+    public void test3Is3() {
+
+    }
+}
